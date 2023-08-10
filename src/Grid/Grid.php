47aa4ef7458765b6ee916f8d4fa1faa4f3018816
simbr <brown.simon.a@gmail.com>
2023-08-10 18:30:56 +0200

    Improve grid code

--- src/Grid/Grid.php
+++ src/Grid/Grid.php
@@ -9,55 +9,61 @@ class Grid
     /**
      * @var array
      */
-    private $config;
+    private array $config;
 
     /**
      * @var array
      */
-    private $actionTemplates;
+    private array $actionTemplates;
 
     /**
      * @var int
      */
-    private $pageSize;
+    private int $pageSize;
 
     /**
      * @var array
      */
-    private $fieldTemplates;
+    private array $fieldTemplates;
 
     /**
      * @var array
      */
-    private $sorting = [];
+    private array $sorting = [];
 
     /**
      * @var array
      */
-    private $filters = [];
+    private array $filters = [];
 
     /**
      * @var Field[]
      */
-    private $fields = [];
+    private array $fields = [];
 
     /**
      * @var Action[]
      */
-    private $actions = [];
+    private array $actions = [];
 
     /**
      * @var array
      */
-    private $filterTemplates;
+    private array $filterTemplates;
 
     /**
      * @var bool
      */
-    private $deleteConfirmation = true;
+    private bool $deleteConfirmation;
 
     /**
-     * @param array $config
+     * @param FilterRegistry $filterRegistry
+     * @param array          $config
+     * @param array          $actionTemplates
+     * @param array          $fieldTemplates
+     * @param array          $filterTemplates
+     *
+     * @throws \ReflectionException
      */
     public function __construct(FilterRegistry $filterRegistry, array $config = [], array $actionTemplates = [], array $fieldTemplates = [], array $filterTemplates = [])
     {
@@ -69,47 +75,11 @@ class Grid
         $this->sorting = $this->config['grid']['sorting'] ?? [];
         $this->deleteConfirmation = $this->config['config']['delete_confirmation'] ?? true;
 
-        $filters = $this->config['grid']['filters'] ?? [];
-        $fields = $this->config['grid']['fields'] ?? [];
-        $actions = $this->config['grid']['actions'] ?? [];
-
         $className = strtolower((new \ReflectionClass($this->config['config']['class']))->getShortName());
 
-        foreach ($filters as $key => $data) {
-            $filter = new Filter();
-            $filter->setName($key);
-            $filter->setLabel($data['label'] ?? 'sherlockode_crud.filter.' . $this->camelCaseToSnakeCase($key));
-            $filter->setType($data['type']);
-            $filter->setFilterType($filterRegistry->get($data['type'])->getFormType());
-            $filter->setTemplate($this->filterTemplates[$data['type']]);
-            $filter->setOptions(array_merge($data['options'], ['label' => 'sherlockode_crud.filter.'.$key]));
-
-            $this->filters[$key] = $filter;
-        }
-
-        foreach ($fields as $key => $data) {
-            $field = new Field();
-            $field->setKey($key);
-            $field->setLabel($data['label'] ?? 'sherlockode_crud.' . $className . '.' . $this->camelCaseToSnakeCase($key));
-            $field->setOptions($data['options'] ?? []);
-            $field->setSortable(array_key_exists('sortable', $data));
-            $field->setPath($data['path'] ?? null);
-
-            if (isset($data['type']) && isset($this->fieldTemplates[$data['type']])) {
-                $field->setTemplate($this->fieldTemplates[$data['type']]);
-            }
-
-            $this->fields[$key] = $field;
-        }
-
-        foreach ($actions as $key => $data) {
-            if (isset($this->actionTemplates[$key])) {
-                $action = new Action();
-                $action->setTemplate($this->actionTemplates[$key]);
-
-                $this->actions[$key] = $action;
-            }
-        }
+        $this->generateFilters($filterRegistry, $this->config['grid']['filters'] ?? []);
+        $this->generateFields($this->config['grid']['fields'] ?? [], $className);
+        $this->generateActions($this->config['grid']['actions'] ?? []);
     }
 
     /**
@@ -200,12 +170,74 @@ class Grid
     }
 
     /**
-     * @param $string
+     * @param string $string
      *
      * @return string
      */
-    private function camelCaseToSnakeCase($string): string
+    private function camelCaseToSnakeCase(string $string): string
     {
         return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $string));
+    }
+
+    /**
+     * @param FilterRegistry $filterRegistry
+     * @param array          $filters
+     *
+     * @return void
+     */
+    private function generateFilters(FilterRegistry $filterRegistry, array $filters): void
+    {
+        foreach ($filters as $key => $data) {
+            $filter = new Filter();
+            $filter->setName($key);
+            $filter->setLabel($data['label'] ?? 'sherlockode_crud.filter.' . $this->camelCaseToSnakeCase($key));
+            $filter->setType($data['type']);
+            $filter->setFilterType($filterRegistry->get($data['type'])->getFormType());
+            $filter->setTemplate($this->filterTemplates[$data['type']]);
+            $filter->setOptions(array_merge($data['options'], ['label' => 'sherlockode_crud.filter.'.$key]));
+
+            $this->filters[$key] = $filter;
+        }
+    }
+
+    /**
+     * @param array  $fields
+     * @param string $className
+     *
+     * @return void
+     */
+    private function generateFields(array $fields, string $className): void
+    {
+        foreach ($fields as $key => $data) {
+            $field = new Field();
+            $field->setKey($key);
+            $field->setLabel($data['label'] ?? 'sherlockode_crud.' . $className . '.' . $this->camelCaseToSnakeCase($key));
+            $field->setOptions($data['options'] ?? []);
+            $field->setSortable(array_key_exists('sortable', $data));
+            $field->setPath($data['path'] ?? null);
+
+            if (isset($data['type']) && isset($this->fieldTemplates[$data['type']])) {
+                $field->setTemplate($this->fieldTemplates[$data['type']]);
+            }
+
+            $this->fields[$key] = $field;
+        }
+    }
+
+    /**
+     * @param array $actions
+     *
+     * @return void
+     */
+    private function generateActions(array $actions): void
+    {
+        foreach ($actions as $key => $data) {
+            if (isset($this->actionTemplates[$key])) {
+                $action = new Action();
+                $action->setTemplate($this->actionTemplates[$key]);
+
+                $this->actions[$key] = $action;
+            }
+        }
     }
 }
